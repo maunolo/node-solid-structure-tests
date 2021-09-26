892e8ef5ce5b56c3fe3cd1256833ced36559b6c4
Mauricio Lopes <mauricionlopes.dev@gmail.com>
2021-09-26 20:46:21 -0300

    finishing tests specifications

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase'
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers
+
+      const users = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString()
+      })
+
+      return response.json(users)
+    } catch (error) {
+      return response.status(400).json({ error })
+    }
   }
 }
 
-export { ListAllUsersController };
+export { ListAllUsersController }
